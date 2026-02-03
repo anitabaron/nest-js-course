8fe7936b31b82946b2fdfa70cf404919fc6a3543
Anita Baron <anita.baron@i4b.pl>
2026-02-03 09:01:08 +0100

    CRUD methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,12 +1,79 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get() findAll() {
-    return 'All coffees';
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  // @Get()
+  // findAll() {
+  //   return 'All coffees';
+  // }
+
+  // @Get()
+  // findAll(@Query() paginationQuery) {
+  //   const { limit, offset } = paginationQuery;
+  //   return `All coffees. Limit: ${limit}, Offset: ${offset}`;
+  // }
+  // localhost:3000/coffees?limit=5&offset=10
+
+  @Get()
+  findAll() {
+    return this.coffeesService.findAll();
+  }
+  // @Get()
+  // findAll(@Res() response) {
+  //   return response.status(200).send('All coffees');
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return `Coffee id: ${id}`;
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(id);
   }
 
-  @Get(':id') findOne(@Param('id') id: string) {
-    return `Coffee id: ${id}`;
+  // @Post()
+  // // @HttpCode(HttpStatus.GONE)
+  // create(@Body() body) {
+  //   return body;
+  // }
+
+  @Post()
+  // @HttpCode(HttpStatus.GONE)
+  create(@Body() createCoffeeDto: any) {
+    return this.coffeesService.create(createCoffeeDto);
+  }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() body) {
+  //   return `This update #${id} coffee`;
+  // }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: any) {
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return `This delete #${id} coffee`;
+  // }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
   }
 }
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Coffee 1',
+      brand: 'Brand 1',
+      flavors: ['flavor 1', 'flavor 2'],
+    },
+  ];
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      existingCoffee.name = updateCoffeeDto.name;
+      existingCoffee.brand = updateCoffeeDto.brand;
+      existingCoffee.flavors = updateCoffeeDto.flavors;
+    }
+    return existingCoffee;
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
